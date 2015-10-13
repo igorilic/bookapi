497cd0ef597458b1f620e2b7139f732b5bed86dc
Igor Ilić <ilicigor@outlook.com>
2015-10-13 22:30:30 +0200

    get, get id, post

--- app.js
+++ app.js
@@ -1,8 +1,67 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
+var db = mongoose.connect('mongodb://localhost:27017/bookAPI');
+
+var Book = require('./models/bookModel');
 var app = express();
 
 var port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var bookRouter = express.Router();
+
+
+bookRouter.use(function(req, res, next) {
+    console.log('Nesto se desava');
+    next();
+});
+
+bookRouter.route('/Books')
+    .post(function(req, res) {
+        var book = new Book(req.body);
+
+        book.save(function(err) {
+            if (err) {
+                res.status(400).send(err);
+            } else {
+                res.json(book);
+            }
+        });
+
+        res.send(book);
+
+    })
+    .get(function(req, res) {
+
+        var query = {};
+        if (req.query.genre) {
+            query.genre = req.query.genre;
+        }
+        Book.find(query, function(err, books) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.json(books);
+            }
+        });
+    });
+
+bookRouter.route('/Books/:bookId')
+    .get(function(req, res) {
+        Book.findById(req.params.bookId, function(err, book) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.json(book);
+            }
+        });
+    });
+
+app.use('/api', bookRouter);
 
 app.get('/', function(req, res) {
 	res.send('Welcome!');
