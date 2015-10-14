52944d1f1b9552f4fe3c8ad0a422b01c00782f0a
Igor Ilic <ilicigor@outlook.com>
2015-10-14 08:26:29 +0200

    app

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
 var express = require('express'),
-	mongoose = require('mongoose');
+	mongoose = require('mongoose'),
+	bodyParser = require('body-parser');
 	
-var db = mongoose.connect('mongodb://localhost/bookAPI', function(err) {
-	if (err) {
-		console.log(err);
-	}
-});
+var db = mongoose.connect('mongodb://localhost:27017/bookAPI');
+// var db = mongoose.connection;
+// db.on('error', function(err) {
+// 	console.log(err.message);
+// });
+// db.once('open', function() {
+// 	console.log('Mongo is connected');
+// });
 var Book = require('./models/bookModel');
 
 var app = express();
 
 var port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 var bookRouter = express.Router();
 
-bookRouter.route('/books')
+bookRouter.route('/Books')
+	.post(function(req, res) {
+		var book = new Book(req.body);
+		res.send(book);
+	})
 	.get(function(req, res) {
-		Book.find(function(err, books) {
+		
+		var query = {};
+		
+		if (req.query.genre) {
+			query.genre  = req.query.genre;
+		}
+		Book.find(query, function(err, books) {
 			if (err) {
 				res.status(500).send(err);
 			} else {
@@ -24,7 +41,19 @@ bookRouter.route('/books')
 			}
 		});
 	});
-
+bookRouter.route('/Books/:bookId')
+	.get(function(req, res) {
+			
+	 	Book.findById(req.params.bookId, function(err, book) {
+			if (err) {
+				res.status(500).send(err);
+			} else {
+				res.json(book);
+			}
+		});
+	});
+		
+		
 app.use('/api', bookRouter);
 
 
